9a1c4ccac00434751a632033798daa5ae0eaaf7c
Ariel Leyva <aleyva1509@gmail.com>
2023-12-04 21:17:40 -0600

    add: cors and fetch htmx-rust

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,9 @@
+use rocket::http::Method;
+use rocket::{get, routes};
 
 #[macro_use] extern crate rocket;
 use rocket_dyn_templates::{Template, context};
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
 
 #[get("/")]
 fn index() -> Template {
@@ -17,7 +20,18 @@ fn idtest(id: usize) -> String {
 
 #[launch]
 fn rocket() -> _ {
+    let allowed_origins = AllowedOrigins::some_exact(&[
+        "http://localhost:3000", "https://hiive.github.io"]);
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }.to_cors().unwrap();
+
     rocket::build()
         .mount("/", routes![index, idtest])
-        .attach(Template::fairing())    
+        .attach(Template::fairing())
+        .attach(cors)
 }
